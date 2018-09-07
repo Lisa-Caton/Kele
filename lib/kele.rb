b9a684a00171e3c05a171c088f7a47d3179f0b9b
Lisa Caton <lisacaton01@gmail.com>
2018-09-07 09:46:23 -0400

    messages complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,14 +1,22 @@
 require 'httparty'
 require 'json'
 require './lib/roadmap'
+require './lib/messages'
 
 class Kele
   include HTTParty
-  require Roadmap
+  include Roadmap
+  include Messages
 
   # Creates a new Kele client authorized with a username and password
   # Params: username = string, password = string
   def initialize(email, password)
+
+    # Bloc's API URL
+    # Params: endpoint = string
+    def api_url(endpoint)
+      "https://www.bloc.io/api/v1/#{endpoint}"
+    end
 
     # Use the httparty class method .post to send a post request to the sessions endpoint of Bloc’s API with the email and password in the body of the request.
     response = self.class.post(api_url("sessions"), body: {"email": email, "password": password})
@@ -19,11 +27,7 @@ class Kele
     # If the username and password are invalid, Bloc's API will not return an authorization token.
     puts "There was a problem authorizing those credentials. Please try again." if @auth_token.nil?
   end
-  # Bloc's API URL
-  # Params: endpoint = string
-  def api_url(endpoint)
-    "https://www.bloc.io/api/v1/#{endpoint}"
-  end
+
 
   # Retrieve's the current user as a JSON blob by passing auth_token to the request to property authenticate against the Bloc API.
   # Params: auth_token = string
