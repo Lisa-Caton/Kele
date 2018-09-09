bc1ff4c5c7ee53e00f1c5ee8b05b03f7c9e9fbf4
Lisa Caton <lisacaton01@gmail.com>
2018-09-09 11:17:29 -0400

    updated get_me method

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,34 +1,63 @@
 require 'httparty'
 require 'json'
+require './lib/roadmap'
+require './lib/messages'
 
 class Kele
   include HTTParty
+  include Roadmap
+  include Messages
 
   # Creates a new Kele client authorized with a username and password
   # Params: username = string, password = string
   def initialize(email, password)
 
+    # Bloc's API URL
+    # Params: endpoint = string
+    def api_url(endpoint)
+      'https://www.bloc.io/api/v1/#{endpoint}'
+    end
+
     # Use the httparty class method .post to send a post request to the sessions endpoint of Bloc’s API with the email and password in the body of the request.
-    response = self.class.post(api_url("sessions"), body: {"email": email, "password": password})
+    response = self.class.post(api_url('sessions'),
+    body: {'email': email, 'password': password})
 
     # The @auth_token instance variable holds the authorization token provided by Bloc's API upon verifying successful username and password.
-    @auth_token = response["auth_token"]
+    @auth_token = response['auth_token']
 
     # If the username and password are invalid, Bloc's API will not return an authorization token.
-    puts "There was a problem authorizing those credentials. Please try again." if @auth_token.nil?
+    puts 'There was a problem authorizing those credentials. Please try again.' if @auth_token.nil?
   end
-  # Bloc's API URL
-  # Params: endpoint = string
-  def api_url(endpoint)
-    "https://www.bloc.io/api/v1/#{endpoint}"
-  end
+
 
   # Retrieve's the current user as a JSON blob by passing auth_token to the request to property authenticate against the Bloc API.
   # Params: auth_token = string
   # Use the httparty class method .get to pass the auth_token to Bloc's API with httparty's header option.
   def get_me
-    response = self.class.get(url, headers: { "authorization" => @auth_token })
+    response = self.class.get(api_url('users/me'),
+    headers: { 'authorization' => @auth_token })
+
     # Parse the JSON document returned in the response into a Ruby hash
-    JSON.parse(body.response)
+     JSON.parse(response.body)
   end
+
+  #Retrieve the availability of the current user’s mentor
+  def get_mentor_availability(mentor_id)
+    # Point the HTTParty GET method at the mentors/mentor_id/student_availability endpoint of Bloc’s API.
+    # Use HTTParty’s header option to pass the auth_token.
+    response = self.class.get('/mentors/#{mentor_id}/student_availability',
+    headers: { “authorization” => @auth_token })
+    # This is the array that will hold all of the time slots that are not booked.
+    available = []
+    # Parse the JSON document returned in the response into a Ruby hash.
+    # Loop through each of the mentor’s time slots. If the booked attribute is null, or the time slot is available, add that time slot to the available array.
+    JSON.parse(response.body).each do |time_slot|
+      if time_slot[“booked”].nil?
+       available << time_slot
+      end
+    end
+    # Return all of the available time slots.
+    available
+  end
+
 end
---
+++ lib/messages.rb
@@ -0,0 +1,24 @@
+module Messages
+  def get_messages
+  end
+
+  def create_message
+      # Point the HTTParty POST method at the messages endpoint of Bloc’s API.
+      # Use HTTParty’s headers option to pass auth_token to the request to properly authenticate against the Bloc API.
+      # Use the HTTParty’s body option to pass all of the attributes to the request (sender, recipient_id, token, subject, stripped-text)
+      response = self.class.post(api_url("messages"),
+      headers: { “authorization” => @auth_token },
+      body:{
+        "sender" => sender,
+        "recipient_id" => recipient_id,
+        "token" => token,
+        "subject" => subject,
+        "stripped-text" => stripped-text
+      })
+
+      @auth_token = response["auth_token"]
+
+      puts "You're message was sent!" if response.success?
+      # Then let’s return a message if the response is successful.
+  end
+end
