a9bc462fc4117d9e09d212a4b967d139cc8bb7c0
Lisa Caton <lisacaton01@gmail.com>
2018-09-09 11:35:46 -0400

    updating merge

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,34 +1,21 @@
-
 module Roadmap
   def get_roadmap(chain_id)
-<<<<<<< HEAD
-    response = self.class.get(api_url('/roadmaps/#{chain_id}'), headers: { “authorization” => @auth_token })
-=======
     response = self.class.get(api_url('/roadmaps/#{chain_id}'),
     headers: { “authorization” => @auth_token })
->>>>>>> 2-Rails-retrieve-users
 
     JSON.parse(response.body)
   end
 
   def get_checkpoint(checkpoint_id)
-<<<<<<< HEAD
-    response = self.class.get(api_url('/checkpoints/#{checkpoint_id}'), headers: { “authorization” => @auth_token })
-=======
     response = self.class.get(api_url('/checkpoints/#{checkpoint_id}'),
     headers: { “authorization” => @auth_token })
->>>>>>> 2-Rails-retrieve-users
 
     JSON.parse(response.body)
   end
 
   def get_remaining_checkpoints(chain_id)
-<<<<<<< HEAD
-    response = self.class.get(api_url('/checkpoints/#{chain_id}'), headers: { “authorization” => @auth_token })
-=======
     response = self.class.get(api_url('/checkpoints/#{chain_id}'),
     headers: { “authorization” => @auth_token })
->>>>>>> 2-Rails-retrieve-users
 
     JSON.parse(response.body)
   end
