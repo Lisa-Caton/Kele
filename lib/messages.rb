a1473a67948c7f0b33c8ba09e217dd5d7c3b6ae1
Lisa Caton <lisacaton01@gmail.com>
2018-09-09 12:08:07 -0400

    fixing quotes

--- lib/messages.rb
+++ lib/messages.rb
@@ -7,7 +7,7 @@ module Messages
       # Use HTTParty’s headers option to pass auth_token to the request to properly authenticate against the Bloc API.
       # Use the HTTParty’s body option to pass all of the attributes to the request (sender, recipient_id, token, subject, stripped-text)
       response = self.class.post(api_url("messages"),
-      headers: { “authorization” => @auth_token },
+      headers: { "authorization" => @auth_token },
       body:{
         "sender" => sender,
         "recipient_id" => recipient_id,
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,21 +1,21 @@
 module Roadmap
   def get_roadmap(chain_id)
     response = self.class.get(api_url("/roadmaps/#{chain_id}"),
-    headers: { “authorization” => @auth_token })
+    headers: { "authorization" => @auth_token })
 
     JSON.parse(response.body)
   end
 
   def get_checkpoint(checkpoint_id)
     response = self.class.get(api_url("/checkpoints/#{checkpoint_id}"),
-    headers: { “authorization” => @auth_token })
+    headers: { "authorization" => @auth_token })
 
     JSON.parse(response.body)
   end
 
   def get_remaining_checkpoints(chain_id)
     response = self.class.get(api_url("/checkpoints/#{chain_id}"),
-    headers: { “authorization” => @auth_token })
+    headers: { "authorization" => @auth_token })
 
     JSON.parse(response.body)
   end
